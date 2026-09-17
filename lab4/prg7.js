7ddca74fc0e7d73db1c39e5f9b617a76db5f2a4c
Akshat Agarwal <akshat.agg2008@gmail.com>
2026-09-17 12:18:17 +0530

    js

--- lab4/prg7.js
+++ lab4/prg7.js
@@ -1,29 +1,35 @@
-import http from 'http';
+import http from "http";
+import { getUsers } from "./users.js";
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/api/users',req.method === 'GET') {
-    res.end(JSON.stringify({msg: "all users" }));
-  }
+  if (req.url === "/api/users" && req.method === "GET") {
+    res.end(JSON.stringify(getUsers()));
+  } 
 
-  else if (req.url === '/api/users',req.method === 'POST') {
-    res.end(JSON.stringify({msg: "add users" }));
+
+  else if (req.url === "/api/users" && req.method === "POST") {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk;
+   })
+   req.on('end',() => {
+    const user = JSON.parse(body);
+    console.log(user);
+   })
+  
+    res.end(JSON.stringify({ msg: "add user" }));
   }
-  else if (req.url === '/api/users',req.method === 'PUT') {
-    res.end(JSON.stringify({msg: "update user 1" }));
-  }
-  else if (req.url === '/api/users',req.method === 'DELETE') {
-    res.end(JSON.stringify({msg: "remove user 1" }));
-  }
-  else if (req.url === '/api/users',req.method === 'PATCH') {
-    res.end(JSON.stringify({msg: "" }));
-  }
-  else {
+  
+  else if (req.url === "/api/users/1" && req.method === "GET") {
+    res.end(JSON.stringify({ msg: "single user with id 1" }));
+  } else if (req.url === "/api/users/1" && req.method === "PUT") {
+    res.end(JSON.stringify({ msg: "update user 1" }));
+  } else if (req.url === "/api/users/1" && req.method === "DELETE") {
+    res.end(JSON.stringify({ msg: "remove 1" }));
+  } else {
     res.statusCode = 404;
     res.end();
   }
-
 });
 
-server.listen(3030, () => {
-  console.log('Server prg7 running ');
-});
+server.listen(3030, () => console.log("prg7 is running"));
--- user.js
+++
@@ -1,4 +0,0 @@
-//we use in memory database
-let users = [
-    {id: 1, name: "John", age: 30},
-];
